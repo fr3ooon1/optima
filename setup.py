73da3ed821592b5c1a63e9bc67e4d8e79d115ee4
Muhammad Essam <141019848+fr3ooon1@users.noreply.github.com>
2025-10-01 16:26:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-# from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-# with open("requirements.txt") as f:
-# 	install_requires = f.read().strip().split("\n")
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
 
-# # get version from __version__ variable in optima/__init__.py
-# from optima import __version__ as version
+# get version from __version__ variable in optima/__init__.py
+from optima import __version__ as version
 
-# setup(
-# 	name="optima",
-# 	version=version,
-# 	description="ERPNext OPtimizations",
-# 	author="IT Systematic",
-# 	author_email="Sales@itsystematic.com",
-# 	packages=find_packages(),
-# 	zip_safe=False,
-# 	include_package_data=True,
-# 	install_requires=install_requires
-# )
+setup(
+	name="optima",
+	version=version,
+	description="ERPNext OPtimizations",
+	author="IT Systematic",
+	author_email="Sales@itsystematic.com",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
